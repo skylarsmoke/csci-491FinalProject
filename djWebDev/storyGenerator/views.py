ebe0fc937158a77e8bde847b3bc50c59abb0bfe7
KadeR-jpg <54875162+KadeR-jpg@users.noreply.github.com>
2020-10-27 18:29:41 -0600

    Messing around with adding html and css to the page

--- djWebDev/storyGenerator/views.py
+++ djWebDev/storyGenerator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'storyGenerator/index.html')
+
+
+
